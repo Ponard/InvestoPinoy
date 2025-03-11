89b3fb2fce23a7675681e7984d6640593284ffcd
aiurkive <easato@mymail.mapua.edu.ph>
2025-03-11 23:01:55 +0900

    Updated login UI and regenerated ui_login.py

--- QtGUI/ui_login.py
+++ QtGUI/ui_login.py
@@ -23,7 +23,7 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(800, 600)
+        Form.resize(960, 540)
         palette = QPalette()
         brush = QBrush(QColor(0, 0, 0, 255))
         brush.setStyle(Qt.SolidPattern)
@@ -101,38 +101,155 @@ class Ui_Form(object):
 #endif
         palette.setBrush(QPalette.Disabled, QPalette.Accent, brush2)
         Form.setPalette(palette)
+        font = QFont()
+        font.setFamilies([u"Segoe UI"])
+        font.setBold(True)
+        Form.setFont(font)
+        Form.setStyleSheet(u"")
         self.horizontalLayout = QHBoxLayout(Form)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.frame = QFrame(Form)
         self.frame.setObjectName(u"frame")
+        self.frame.setStyleSheet(u"#frame {\n"
+"	border: 10px solid white;\n"
+"	border-radius: 50%;\n"
+"}")
         self.frame.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame.setFrameShadow(QFrame.Shadow.Raised)
         self.verticalLayout = QVBoxLayout(self.frame)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_2)
+
         self.login_header = QLabel(self.frame)
         self.login_header.setObjectName(u"login_header")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.login_header.sizePolicy().hasHeightForWidth())
+        self.login_header.setSizePolicy(sizePolicy)
+        font1 = QFont()
+        font1.setFamilies([u"Futura"])
+        font1.setBold(True)
+        self.login_header.setFont(font1)
+        self.login_header.setStyleSheet(u"font-size: 36px;\n"
+"color: #432d60;")
         self.login_header.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.login_header.setWordWrap(True)
 
         self.verticalLayout.addWidget(self.login_header)
 
-        self.vertical_spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        self.login_header_2 = QLabel(self.frame)
+        self.login_header_2.setObjectName(u"login_header_2")
+        font2 = QFont()
+        font2.setFamilies([u"Al Nile"])
+        font2.setPointSize(18)
+        font2.setBold(True)
+        self.login_header_2.setFont(font2)
+        self.login_header_2.setStyleSheet(u"color: #706a7a;")
+        self.login_header_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.login_header_2.setWordWrap(True)
 
-        self.verticalLayout.addItem(self.vertical_spacer)
+        self.verticalLayout.addWidget(self.login_header_2)
 
+        self.verticalSpacer = QSpacerItem(20, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
         self.login_username = QLineEdit(self.frame)
         self.login_username.setObjectName(u"login_username")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.login_username.sizePolicy().hasHeightForWidth())
+        self.login_username.setSizePolicy(sizePolicy1)
+        self.login_username.setMinimumSize(QSize(200, 0))
+        self.login_username.setAutoFillBackground(False)
+        self.login_username.setStyleSheet(u"QLineEdit {\n"
+"    background-color: rgba(60, 54, 82, 204);\n"
+"    border: 2px solid transparent;\n"
+"}\n"
+"QLineEdit:hover {\n"
+"    border: 2px solid rgba(60,54,82,204);\n"
+"}\n"
+"QLineFocus:focus {\n"
+"	border: 2px solid black;\n"
+"}")
 
-        self.verticalLayout.addWidget(self.login_username)
+        self.horizontalLayout_4.addWidget(self.login_username)
 
+
+        self.verticalLayout.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.login_password = QLineEdit(self.frame)
         self.login_password.setObjectName(u"login_password")
+        sizePolicy1.setHeightForWidth(self.login_password.sizePolicy().hasHeightForWidth())
+        self.login_password.setSizePolicy(sizePolicy1)
+        self.login_password.setMinimumSize(QSize(200, 0))
+        self.login_password.setStyleSheet(u"QLineEdit {\n"
+"    background-color: rgba(60, 54, 82, 204);\n"
+"    border: 2px solid transparent;\n"
+"}\n"
+"QLineEdit:hover {\n"
+"    border: 2px solid rgba(60,54,82,204);\n"
+"}\n"
+"QLineFocus:focus {\n"
+"	border: 2px solid black;\n"
+"}")
+        self.login_password.setEchoMode(QLineEdit.EchoMode.Password)
 
-        self.verticalLayout.addWidget(self.login_password)
+        self.horizontalLayout_3.addWidget(self.login_password)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
 
         self.login_button = QPushButton(self.frame)
         self.login_button.setObjectName(u"login_button")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.login_button.sizePolicy().hasHeightForWidth())
+        self.login_button.setSizePolicy(sizePolicy2)
+        self.login_button.setMinimumSize(QSize(100, 25))
+        font3 = QFont()
+        font3.setBold(True)
+        self.login_button.setFont(font3)
+        self.login_button.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.login_button.setStyleSheet(u"QPushButton\n"
+"{\n"
+"	background-color: rgb(60,54,82);\n"
+"	border: 1px solid white;\n"
+"	border-radius: 5px;\n"
+"}\n"
+"QPushButton::hover\n"
+"{\n"
+"	background-color: rgba(60,54,82,204);\n"
+"}")
+        self.login_button.setFlat(False)
 
-        self.verticalLayout.addWidget(self.login_button)
+        self.horizontalLayout_2.addWidget(self.login_button)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_3)
 
 
         self.horizontalLayout.addWidget(self.frame)
@@ -140,12 +257,18 @@ class Ui_Form(object):
 
         self.retranslateUi(Form)
 
+        self.login_button.setDefault(False)
+
+
         QMetaObject.connectSlotsByName(Form)
     # setupUi
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.login_header.setText(QCoreApplication.translate("Form", u"InvestoPinoy Insurance Management System", None))
-        self.login_button.setText(QCoreApplication.translate("Form", u"Log in", None))
+        self.login_header.setText(QCoreApplication.translate("Form", u"INVESTOPINOY", None))
+        self.login_header_2.setText(QCoreApplication.translate("Form", u"INSURANCE MANAGEMENT SYSTEM", None))
+        self.login_username.setText("")
+        self.login_password.setPlaceholderText("")
+        self.login_button.setText(QCoreApplication.translate("Form", u"Login", None))
     # retranslateUi
 
